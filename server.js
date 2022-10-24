aa3b929c039ca42a80a7235f5e94ff3651c91223
Xiaodanguo2020 <guo.xiaodan.bk@gmail.com>
2022-10-24 16:59:05 +0200

    Setup routes and end points for products

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const productRoute = require("./routes/product");
+const Product = require("./models").product;
+const Category = require("./models").category;
+
+app.use(express.json());
+
+app.use("/products", productRoute);
+
+// app.get("/prods", async (req, res, next) => {
+//   const testProducts = await Product.findAll({
+//     include: [{ model: Category }],
+//   });
+//   res.send(testProducts);
+//   console.log(testProducts);
+// });
+
+// app.get("/hello", (req, res, next) => {
+//   res.send("hello world");
+// });
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
