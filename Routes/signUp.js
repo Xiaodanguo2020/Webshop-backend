fbd8f5f25eb07fb2a93ff2462a10af118fddbe64
Xiaodanguo2020 <guo.xiaodan.bk@gmail.com>
2022-10-27 11:08:26 +0200

    Updated login function in the back

--- Routes/signUp.js
+++ Routes/signUp.js
@@ -6,11 +6,14 @@ const bcrypt = require("bcrypt");
 router.post("/signup", async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
+
+    console.log(name, email, password, "this is the data");
+
     if (!name || !email || !password) {
       res.status(404).send("Missing parameters");
-    } else if (email.toLowerCase() === email) {
-      res.status(404).send("Name already exists");
     } else {
+      console.log("reached here");
+
       const newUser = await User.create({
         name,
         email,
